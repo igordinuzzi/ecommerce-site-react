b2a711c23b80b9368171bffec7ee68a7a7196c1d
Igor Dinuzzi <75853437+igordinuzzi@users.noreply.github.com>
2024-02-05 16:13:43 +0100

    Fix HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import ProductList from './components/ProductList/ProductList';
